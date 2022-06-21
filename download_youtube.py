96ac74121c5aa66174e0d5f35ec302327fa3d3a4
Vladimir Podaruev <vladimir.podaruev@gmail.com>
2022-06-22 01:03:16 +0300

    Add lots of details and enhancements. Not fully work yet

--- download_youtube.py
+++ download_youtube.py
@@ -1,7 +1,10 @@
 import argparse
+import subprocess as sp
+import json
 
 from pathlib import Path
-import subprocess as sp
+from urllib.parse import urlparse
+from pathvalidate import sanitize_filename
 
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
@@ -9,19 +12,145 @@ from PyQt6.QtCore import *
 import sys
 
 
+def to_hhmmss(seconds, delim=":"):
+    seconds = int(seconds)
+    minutes = seconds // 60
+    hours = minutes // 60
+    d = f"{seconds - 60*minutes:02d}"
+    if minutes:
+        d = f"{minutes - 60*hours:02d}" + delim + d
+    if hours:
+        d = f"{hours:02d}" + delim + d
+    return d
+
+
+def to_seconds(hhmmss):
+    t = hhmmss.rsplit(":,.'")
+    if len(t) == 1:
+        return hhmmss
+    elif len(t) == 2:
+        mm, ss = t
+        return str(int(mm)*60 + int(ss))
+    hh, mm, ss = t
+    return str((int(hh)*60 + int(mm))*60 + int(ss))
+
+
+class NotYoutubeURL(RuntimeError):
+    def __init__(self, url):
+        super().__init__(f"'{url}' is not a youtube URL")
+
+
+class YoutubeVideo:
+    def __init__(self, url):
+        netloc = urlparse(url).netloc
+        if all([item not in netloc for item in {"youtube.com", "youtu.be"}]):
+            raise NotYoutubeURL(url)
+        p = sp.run([f"{args.youtube_dl}", "--dump-json", f"{url}"],
+                    capture_output=True, encoding="utf-8", check=True)
+        js = json.loads (p.stdout.strip())
+        self.url = url
+        self.title = js["title"]  if "title" in js  else "Video Title"
+        self.channel = js["channel"]  if "channel" in js  else "Youtube Channel"
+        self.duration = to_hhmmss(js["duration"])  if "duration" in js  else "0"
+    
+    def download_urls(self):
+        p = sp.run([f"{args.youtube_dl}", "-g", f"{self.url}"],
+                    capture_output=True, encoding="utf-8", check=True)
+        return p.stdout.split()
+
+
+def download(urls, start, end, filename):
+    try :
+        sp.run ([f"{args.ffmpeg}", "-y", "-loglevel", "quiet",
+                 "-ss", f"{start}", "-to", f"{end}", "-i", f"{urls[0]}",
+                 "-ss", f"{start}", "-to", f"{end}", "-i", f"{urls[1]}",
+                 "-c", "copy", f"{filename}"],
+                 stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL, check=True)
+    except sp.SubprocessError as e :
+        with open("err.log", "a") as log :
+            print (e, file=log)
+
+
 class MainWindow(QMainWindow):
-    def __yt_url (self):
+    def __init__(self, *args, **kwargs):
+        super(QMainWindow, self).__init__(*args, **kwargs)
+        
+        mainLayout = QVBoxLayout()
+        mainLayout.addLayout(self.__yt_url())
+        mainLayout.addWidget(self.__yt_title(),
+                             alignment=Qt.AlignmentFlag.AlignCenter)
+        mainLayout.addLayout(self.__excerpt())
+        mainLayout.addLayout(self.__save_as())
+        mainLayout.addWidget(self.__download())
+        mainLayout.addWidget(self.__about(),
+                             alignment=Qt.AlignmentFlag.AlignRight)
+        widget = QWidget()
+        widget.setLayout(mainLayout)
+        
+        self.set_step1_enabled(False)
+        self.set_step2_enabled(False)
+        
+        self.setCentralWidget(widget)
+        self.setWindowTitle("Youtube Cut - Share the positive / Делись позитивом")
+        self.setWindowIcon(QIcon("cs-logo.jpg"))
+    
+    def set_step1_enabled(self, ok):
+        self.fromLineEdit.setEnabled(ok)
+        self.toLineEdit.setEnabled(ok)
+        self.excerptGoPushButton.setEnabled(ok)
+        return
+    
+    def set_step2_enabled(self, ok):
+        self.saveAsLineEdit.setEnabled(ok)
+        self.saveAsPushButton.setEnabled(ok)
+        self.downloadPushButton.setEnabled(ok)
+        return
+    
+    def __yt_title(self):
+        label = QLabel()
+        label.setTextFormat(Qt.TextFormat.RichText)
+        label.hide()
+        self.ytTitle = label
+        return label
+
+    def __yt_url(self):
         label = QLabel("Youtube link:")
         lineEdit = QLineEdit()
         lineEdit.setPlaceholderText("youtube link / ютуб ссылка")
         self.ytUrlLineEdit = lineEdit
         
+        pushButton = QPushButton()
+        pushButton.setIcon(QIcon("go-next.png"))
+        pushButton.setToolTip("Go! / Поехали!")
+        pushButton.clicked.connect(self.yt_url_changed)
+        
         layout = QHBoxLayout()
         layout.addWidget(label)
         layout.addWidget(lineEdit)
+        layout.addWidget(pushButton)
         return layout
     
-    def __excerpt (self):
+    def yt_url_changed(self):
+        url = self.ytUrlLineEdit.text()
+        if not url:
+            return
+        elif url.isspace():
+            self.ytUrlLineEdit.clear()
+            return
+        try:
+            self.ytVideo = YoutubeVideo(url)
+            self.ytTitle.setText("<b>"+ self.ytVideo.channel +"</b>: "+ self.ytVideo.title)
+            self.toLineEdit.setPlaceholderText(self.ytVideo.duration)
+            self.set_step1_enabled(True)
+            self.ytTitle.show()
+        except NotYoutubeURL as e:
+            QMessageBox.warning(self, "Warning", f"'{url}' is not a YouTube URL / это не Ютуб ссылка")
+            self.ytUrlLineEdit.clear()
+        except sp.CalledProcessError as e:
+            QMessageBox.critical(self, "Error", f"{e}\n\n{e.stderr}")
+            self.ytUrlLineEdit.clear()
+    
+    def __excerpt(self):
         fromLabel = QLabel("Cut from:")
         fromLineEdit = QLineEdit()
         fromLineEdit.setPlaceholderText("00:00:00")
@@ -32,13 +161,28 @@ class MainWindow(QMainWindow):
         toLineEdit.setPlaceholderText("00:00:00")
         self.toLineEdit = toLineEdit
         
+        pushButton = QPushButton()
+        pushButton.setIcon(QIcon("go-next.png"))
+        pushButton.setToolTip("Go! / Поехали!")
+        pushButton.clicked.connect(self.excerpt_changed)
+        self.excerptGoPushButton = pushButton
+        
         layout = QHBoxLayout()
         layout.addWidget(fromLabel)
         layout.addWidget(fromLineEdit)
         layout.addSpacing(5)
         layout.addWidget(toLabel)
         layout.addWidget(toLineEdit)
+        layout.addWidget(pushButton)
         return layout
+    
+    def excerpt_changed(self):
+        s, f = self.fromLineEdit.text(), self.toLineEdit.text()
+        s, f = s.replace(":", "."), f.replace(":", ".")
+        p = Path.cwd() / sanitize_filename(self.ytVideo.title)
+        p = p.with_stem(p.stem + f"_{s}-{f}.mp4")
+        self.saveAsLineEdit.setText(str(p))
+        self.set_step2_enabled(True)
     
     def __save_as(self):
         saveAsLabel = QLabel("Save as:")
@@ -49,6 +193,7 @@ class MainWindow(QMainWindow):
         saveAsPushButton = QPushButton()
         saveAsPushButton.setIcon(QIcon("saveAs.png"))
         saveAsPushButton.clicked.connect(self.get_filename)
+        self.saveAsPushButton = saveAsPushButton
         
         layout = QHBoxLayout()
         layout.addWidget(saveAsLabel)
@@ -57,15 +202,22 @@ class MainWindow(QMainWindow):
         return layout
     
     def get_filename(self):
+        dirname = self.saveAsLineEdit.text().strip()
+        if not dirname:
+            dirname = "."
+        elif not Path(dirname).is_dir():
+            dirname = Path(dirname).parent.as_posix()
         file, filter = QFileDialog.getSaveFileName(self, caption="Save As",
-                                                   directory=Path.cwd().as_posix(),
+                                                   directory=dirname,
                                                    filter="Video Files (*.mp4)")
-        self.saveAsLineEdit.setText(file)
+        if file:
+            self.saveAsLineEdit.setText(file)
     
     def __download(self):
         pushButton = QPushButton("Download / Загрузить")
         pushButton.setIcon(QIcon("download.png"))
         pushButton.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        pushButton.setEnabled(False)
         self.downloadPushButton = pushButton
         return pushButton
     
@@ -73,57 +225,17 @@ class MainWindow(QMainWindow):
         label = QLabel("<font color=\"Grey\"><i>Created with love by AllatRa IT team</i></font>")
         label.setTextFormat(Qt.TextFormat.RichText)
         return label
-    
-    def __init__(self, *args, **kwargs):
-        super(QMainWindow, self).__init__(*args, **kwargs)
-        
-        mainLayout = QVBoxLayout()
-        mainLayout.addLayout(self.__yt_url())
-        mainLayout.addLayout(self.__excerpt())
-        mainLayout.addLayout(self.__save_as())
-        mainLayout.addWidget(self.__download())
-        mainLayout.addWidget(self.__about(),
-                             alignment=Qt.AlignmentFlag.AlignRight)
-        widget = QWidget()
-        widget.setLayout(mainLayout)
-        
-        self.setCentralWidget(widget)
-        self.setWindowTitle("Youtube Cut")
-        self.setWindowIcon(QIcon("cs-logo.jpg"))
-
-
-def direct_links(youtube_dl, url):
-    print(">", f"{url}")
-    p = sp.run([f"{youtube_dl}", "-g", f"{url}"], capture_output=True, encoding="utf-8", check=True)
-    return p.stdout.split()
-
-def download(ffmpeg, urls, start, end, name):
-    try :
-        sp.run ([f"{ffmpeg}", "-y", "-loglevel", "quiet",
-                 "-ss", f"{start}", "-to", f"{end}", "-i", f"{urls[0]}",
-                 "-ss", f"{start}", "-to", f"{end}", "-i", f"{urls[1]}",
-                 "-c", "copy", f"{name}"],
-                 stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL, check=True)
-    except sp.SubprocessError as e :
-        with open("err.log", "a") as log :
-            print (e, file=log)
 
 
 if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description="Download files from Youtube.")
-#     parser.add_argument("url", type=str, help="Youtube video link")
-#     parser.add_argument("--start", type=str, help="Start time")
-#     parser.add_argument("--end", type=str, help="End time")
-#     parser.add_argument("--youtube-dl", type=Path, default=Path("dist/youtube-dl"),
-#                         help="path to youtube-dl program [default: %(default)s]")
-#     parser.add_argument("--ffmpeg", type=Path, default=Path("dist/ffmpeg"),
-#                         help="path to ffmpeg program [default: %(default)s]")
-
-#     args = parser.parse_args()
-#     urls = direct_links(args.youtube_dl, args.url)
-#     download(args.ffmpeg, urls, args.start, args.end, "output.mp4")
+    parser = argparse.ArgumentParser(description="Download files from Youtube.")
+    parser.add_argument("--youtube-dl", type=Path, default=Path("youtube-dl"),
+                        help="path to youtube-dl program [default: %(default)s]")
+    parser.add_argument("--ffmpeg", type=Path, default=Path("ffmpeg"),
+                        help="path to ffmpeg program [default: %(default)s]")
 
     app = QApplication(sys.argv)
+    args = parser.parse_args()
 
     window = MainWindow()
     window.show()
