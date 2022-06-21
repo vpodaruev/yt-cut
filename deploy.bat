eb3ddf4edf446b782c2d9a2bf7a139aa13ce0ade
Vladimir Podaruev <vladimir.podaruev@gmail.com>
2022-06-22 01:04:44 +0300

    Deploy script for packaging

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set DIST=dist
+
+pyinstaller --onefile --windowed download_youtube.py
+copy /b ffmpeg.exe %DIST%
+copy /b youtube-dl.exe %DIST%
+copy /b cs-logo.jpg %DIST%
+copy /b download.png %DIST%
+copy /b go.png %DIST%
+copy /b saveAs.png %DIST%
