d520420c42447b4aade2e4a1963f5208e09344dc
Vladimir Podaruev <vladimir.podaruev@gmail.com>
2022-06-27 23:55:42 +0300

    Fix deploy script

--- deploy.bat
+++ deploy.bat
@@ -7,5 +7,6 @@ copy /b ffmpeg.exe %DIST%
 copy /b youtube-dl.exe %DIST%
 copy /b cs-logo.jpg %DIST%
 copy /b download.png %DIST%
-copy /b go.png %DIST%
+copy /b go-next.png %DIST%
+copy /b go-prev.png %DIST%
 copy /b saveAs.png %DIST%
